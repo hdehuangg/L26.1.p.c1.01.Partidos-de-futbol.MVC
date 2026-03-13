66a3dc8c0e52922f1b2ca9116b50211e34f93e18
hdehuangg <diazhuangjhonnraikar@gmail.com>
2026-03-12 21:24:42 -0400

    Delete L26.1.p.c1.13.PartidosFutbol-MVC directory

--- L26.1.p.c1.13.PartidosFutbol-MVC/dist/models/Cl_mJuego.js
+++
@@ -1,8 +0,0 @@
-export default class Cl_mJuego {
-    _resultado;
-    constructor(resultado) {
-        this._resultado = resultado;
-    }
-    set resultado(r) { this._resultado = r; }
-    get resultado() { return this._resultado; }
-}
